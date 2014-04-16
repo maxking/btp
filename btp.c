f07044b152f47c73cc0e8eb53dcee4427560a55e
Abhilash Raj <raj.abhilash1@gmail.com>
2014-04-16 17:13:48 +0530

    code seems to work

--- btp.c
+++ btp.c
@@ -27,76 +27,83 @@ int main() {
   // Required Arrays
 
 
-  Twc = 647;
+  Twc = 647.0;
   Hwnb = 2260000;
   Twb = 99.98 + 273.15;
   Xch2o = 0.05;
-  grate_length = 105;
-  udd_length = 9;
+  grate_length = 105.0;
+  udd_length = 9.0;
   ddd_length = 22.5;
-  rc_length = 78;
-  cz1_length = 90;
-  grate_speed = 2.1/60;
+  rc_length = 78.0;
+  cz1_length = 90.0;
+  grate_speed = 0.035;   // 2.1 m/min
   z = 0.55;
   zstep = 0.01;
-  tstep = 1;
+  tstep = 1.0;
   clf = 0.8;
-  phi = 1;
+  phi = 1.0;
   vg = 0.3833;
   eb = 0.41;
   dc = 7.5e-5;
   ds = 0.012;
   dl = 7.5e-5;
   de = 0.012;
-  //  Ts[0][0] = 25 + 273.15;
   u0 = 1.72e-5;
-  Cu = 113;
+  Cu = 113.0;
   Do2n2 = 0.25;
   rl = dl/2;
   Hc = 28e6;
-  Cco2 = 200;
-  pc = 625;
+  Cco2 = 200.0;
+  pc = 625.0;
   ps = 2.7e3;
-  EL = 10;
+  EL = 10.0;
   el = 0.3;
-  nl = 1000;
+  nl = 1000.0;
   fs = ps;
-  fg = 1300;
+  fg = 1300.0;
   as = 6*(1-eb)/(ds*phi);
-  n =  (int) z/zstep;
+  n =  z/zstep;
   time = (int) grate_length/grate_speed;
   udd_time = (int) udd_length/grate_speed;
 
-  double Ts[n][time], Tg[n], Yh2o[n][time], Yc[n][time], P[n], Xh2o[n], Xo2[n];
+  double Ts[n][time], Xo2[n], Tg[n], Yh2o[n][udd_time], Yc[n][udd_time], P[n], Xh2o[n];
   // Assumed inlet gas temperature for UDD zone
   Tg[0] = 189 + 273.15;
-
+  Xo2[0] = 21.0;
+  Xh2o[0] = 0.056;
+  P[0] = 0;
+  Yh2o[0][0] = 0.131;
   // All solids enter the furnace at room temp i.e. 298K
   int iz, it;
   for(iz=0;iz<n;iz++) {
-    Ts[0][iz] = 298.15;
+      for(it=0;it<udd_time;it++) {
+        Ts[iz][it] = 298.15;
+        Yc[iz][it] = 1.4;
+      }
   }
 
+  printf("UDD_TIME is %d \n", udd_time);
+  printf("Number of elements are %d \n", n);
   // Loop though all the elements of UDD zone
-  for(it=0;it<udd_time;it++) {
+  for(it=0;it<(udd_time-1);it++) {
     //Loop through all the elements in the height for a length of 1s
-    for(iz=0;iz<n;iz++) {
+    for(iz=0;iz<(n-1);iz++) {
       // Compute all variables;
-      Cpg = 881 + 0.31*Tg[iz] - 7.98e-5*pow(Tg[iz],2);
+      Cpg = 881 + 0.31*Tg[iz] - 7.98e-5*pow(Tg[iz], 2);
 
-      ug = u0*pow(Tg[iz]/273.15, 1.5)*((Cu+273.15)/(Cu + Tg[iz]));
+      ug = u0*pow((Tg[iz]/273.15), 1.5)*((Cu+273.15)/(Cu + Tg[iz]));
 
       val = Yh2o[iz][it]/(1-Yh2o[iz][it]);
       pg = 219.38*(1+val)/(0.622 + val);
 
-      Do2c = 0.435e-5*pow((Tg[iz]/298.15), 1.5)/pow((0.29), -9.41);
+      Do2c = 0.435e-5*pow((Tg[iz]/298.15), 1.5)/(pow((0.29), -9.41));
       Dco2 = 7.166666e-10*pow(Tg[iz], 1.75);
       Dsco2 = Dco2*(el/pow(el, -0.41));
 
       Re = de*vg*pg/ug;
       Sc = ug/(pg*Do2n2);
       Sc2 = ug/(pg*Dsco2);
-      Sh = 2 + 0.6*pow((Re/eb), 0.5)*pow(Sc2, (1/3));
+      Sh = 2 + 0.6*pow(Re/eb, 0.5)*pow(Sc2, 0.333);
 
       if (Ts[iz][it] > 1050.15) {
         Cps = 999 + 0.0416*Ts[iz][it];
@@ -108,7 +115,7 @@ int main() {
         Cps = 341.6 + 1.324*Ts[iz][it] - 4.032e-4*pow(Ts[iz][it], 2);
       }
 
-      kpc = 595.6*Ts[iz][it]*exp(-17940/Ts[iz][it]);
+      kpc = 595.6*Ts[iz][it]*exp(-17940.0/Ts[iz][it]);
       kpl = 9.12e3*exp(-40000/(1.987*Ts[iz][it]));
       kmc = Do2c*(2 + 0.6*pow(Sc,(1/3))*pow((Re/eb),(1/2)))/de;
       Kc = kpc*kmc/(kpc+kmc);
@@ -161,7 +168,15 @@ int main() {
       fg = fg + (Rw + Rc)*zstep;
       Yh2o[iz+1][it+1] = Yh2o[it][iz] - Rw*tstep/fs;
       Yc[iz+1][it+1] = Yc[it][iz] - Rc*tstep/fs;
+
     }
   }
+
+  for(iz=0;iz<n;iz++){
+    for(it=udd_time-1;it>=0;it--)
+      printf("%lf ", iz, it, Ts[iz][it]);
+    printf("\n");
+  }
+
   return 0;
 }
